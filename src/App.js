f5e90d552fad5636e32c0ccd26ba37a232d08211
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-07-09 19:32:38 -0300

    fixed api call in useEffect

--- src/App.js
+++ src/App.js
@@ -11,15 +11,16 @@ const App = () => {
   const [query, setQuery] = useState('chicken');
 
   useEffect(() => {
+    const getRecipes = async () => {
+      const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+      const data = await response.json();
+      setRecipes(data.hits);
+      console.log(data.hits);
+    }
     getRecipes();
   }, [query])
 
-  const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-    const data = await response.json();
-    setRecipes(data.hits);
-    console.log(data.hits);
-  }
+
 
   const updateSearch = e => {
     setSearch(e.target.value);
